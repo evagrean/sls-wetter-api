69c0b6bf82b377ffb3e9cd14273737c3b420bd9d
eva <me@example.com>
2020-08-14 12:18:01 +0200

    tested functionality of queryStringParameters with hello function

--- wetter-api/handler.js
+++ wetter-api/handler.js
@@ -1,10 +1,13 @@
 'use strict';
 module.exports.hello = async event => {
+
+  let greeting = event.queryStringParameters.name;
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Hello ${greeting}`,
         input: event,
       },
       null,
