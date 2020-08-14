5d6719578b6178adf3ef33ea2fb9546b1e2dbf8c
eva <me@example.com>
2020-08-14 12:50:45 +0200

    added another code for testing

--- wetter-api/handler.js
+++ wetter-api/handler.js
@@ -1,12 +1,22 @@
+const axios = require('axios');
+
 'use strict';
+
 module.exports.hello = async event => {
+
+  const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?APPID=b13002d47c5a135d4b791e6e956f1137&units=metric&q=Berlin';
+
+  const weatherInfo = await axios.get(weatherURL);
+
+  let temperature = weatherInfo.data.main.temp;
+
+  console.log(weatherInfo.data.temp);
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello World',
-        input: event,
+        message: `The temperature is ${temperature}`
       },
       null,
       2
