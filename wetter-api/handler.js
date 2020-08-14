ea9dad688ed83001c25ae18dbeb8a02ca347517d
eva <me@example.com>
2020-08-14 11:29:50 +0200

    added code for wetter-api service

--- wetter-api/handler.js
+++ wetter-api/handler.js
@@ -1,18 +1,29 @@
+const axios = require('axios');
+
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.getWeather = async event => {
+
+  // City name supplied as path parameter
+  let city = event.pathParameters.cityName;
+
+  // OpenWeatherMap API endpoint
+  // added metric to units query parameter to use Celsius
+  const openweatherURL = 'http://api.openweathermap.org/data/2.5/weather?APPID=b13002d47c5a135d4b791e6e956f1137&units=metric'
+    + `&q=${city}`;
+
+  const weatherInfo = await axios.get(openweatherURL);
+
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
+      { summary: `In ${weatherInfo.data.name} sind es heute ${weatherInfo.data.main.temp} Grad Celsius` },
       null,
       2
     ),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
