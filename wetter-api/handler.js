d2d1438563b3bed802a6b642933ae909037804c4
eva <me@example.com>
2020-08-15 17:55:31 +0200

    fixed error

--- wetter-api/handler.js
+++ wetter-api/handler.js
@@ -55,7 +55,7 @@ module.exports.getTemp = async event => {
       },
       body: JSON.stringify(
         {
-          summary: `Interessiert am Wetter in ${city}, obwohl keine deutsche Stadt? ... Aktuell hier gerade ${temp} °C`
+          summary: `Interessiert am Wetter in ${city}, obwohl keine deutsche Stadt? Aktuell hier gerade ${temp} °C`
         },
       )
     };
