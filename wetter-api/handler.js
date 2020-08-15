caa5c33a618e8eb013e7ee6f0be68c30544d7a0f
eva <me@example.com>
2020-08-15 17:48:38 +0200

    added serverless-offline plugin for testing and refactored code for displaying messages according to given cityName

--- wetter-api/handler.js
+++ wetter-api/handler.js
@@ -4,18 +4,37 @@ const axios = require('axios');
 
 module.exports.getTemp = async event => {
 
-  // City name passed through query string parameter in GET request
-  // Used to make specific request to weather API
-  let cityName = event.queryStringParameters.stadt
+  // City name passed through query string parameter 
+  // If nothing provided or with first visit default is set to 'firstVisit'
 
+  let cityName = 'firstVisit';
+
+  if (event.queryStringParameters && event.queryStringParameters.stadt && event.queryStringParameters.stadt !== "") {
+    cityName = event.queryStringParameters.stadt;
+  }
+
+  // Display first message
+  if (cityName === 'firstVisit') {
+    return {
+      body: JSON.stringify(
+        {
+          message: `Mit den Query String Parametern stadt=[EINE_DEUTSCHE_STADT_DEINER_WAHL] erfährst du die aktuelle Temperatur dort`
+        },
+      ),
+    }
+  }
+
+  // Request URL to weather API
   const apiURL = 'http://api.openweathermap.org/data/2.5/weather?APPID=b13002d47c5a135d4b791e6e956f1137&units=metric&q='
     + `${cityName}`;
-
+  // Get weather info with given city parameter
   const weatherInfo = await axios.get(apiURL);
+
   const city = weatherInfo.data.name;
   const temp = weatherInfo.data.main.temp;
   const country = weatherInfo.data.sys.country;
 
+  // Display weather info 
   if (country === 'DE') {
     return {
       statusCode: 200,
@@ -27,7 +46,7 @@ module.exports.getTemp = async event => {
           summary: `In ${city} hat es heute ${temp} °C`
         },
       ),
-    };
+    }
   } else {
     return {
       statusCode: 200,
@@ -36,7 +55,7 @@ module.exports.getTemp = async event => {
       },
       body: JSON.stringify(
         {
-          summary: `Eigentlich reagiert der Endpoint ja nur auf deutsche Städtenamen... Aber wo wir schon dabei sind: In ${city} hat es gerade ${temp} °C`
+          summary: `Interessiert am Wetter in ${city}, obwohl keine deutsche Stadt? ... Aktuell hier gerade ${temp} °C`
         },
       )
     };
